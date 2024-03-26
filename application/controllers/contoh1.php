8dc28c1453240562832cbc3fb68829dc6167ccf8
AndikaIkbar <dikarayadi@gmail.com>
2024-03-26 07:30:26 +0000

    first commit

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -4,9 +4,6 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "Nama saya Andika Ikbar Rayadi
-        Saya tinggal di daerah Bekasi Timur
-        olahraga yang saya sukai adalah
-        Bersepeda";
+        echo "Nama saya Lintang Pramudya Pamungkas mempunya hbby olahraga";
     }
 }
