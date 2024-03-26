a023131aae25cacc7c750d974484d6def951141b
AndikaIkbar <164613257+AndikaIkbar@users.noreply.github.com>
2024-03-26 07:35:17 +0000

    Update contoh1.php

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -4,6 +4,6 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "Nama saya Lintang Pramudya Pamungkas mempunya hbby olahraga";
+        echo "Nama saya Andika IKbar Rayadi tinggal di bekasi timur hobby saya bersepeda";
     }
 }
